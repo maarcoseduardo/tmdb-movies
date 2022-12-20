e96c8b6de90b0c039eb5153c9467deada6443e8c
Marcos Eduardo <maarcosefb0@gmail.com>
2022-12-20 20:14:01 -0300

    fix: test eslint and request

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,13 +7,8 @@ module.exports = {
   parserOptions: {
     ecmaVersion: 'latest',
   },
-  "extends": ["eslint:recommended", "next"],
   plugins: ['@typescript-eslint'],
   rules: {
-    "@next/next/no-img-element": "off",
-    "react-hooks/exhaustive-deps": "off",
-    "no-undef": "off",
-    "no-unused-vars": "off",
-    "no-redeclare": "off",
+    
   },
 }
