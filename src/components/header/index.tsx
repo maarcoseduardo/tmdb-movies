41bb70c804338f27c3ea73f5aaca7b3cb21d7259
Marcos Eduardo <bacalhau_1995@hotmail.com>
2022-12-26 08:43:19 -0300

    chore: add routing pages in logo

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -7,38 +7,45 @@ import { FloatWishlistMenu } from '../floatWishlistMenu'
 import { useEffect } from 'react'
 import { useMovies } from '../../context/MoviesContext'
 import { api, api_searchedMovies } from '../../services/api'
+import Link from 'next/link'
 
 export function Header() {
   const { handleOpenModalCart } = useModalCart()
   const { handleOpenModalWishlist } = useModalWishlist()
   const { search, setSearch, setMoviesList } = useMovies()
 
-  async function getSearchedMovies(){
-    const searchMovies = await api_searchedMovies.get(`?api_key=${process.env.NEXT_PUBLIC_API_KEY}&query=${search}&page=1`)
+  async function getSearchedMovies() {
+    const searchMovies = await api_searchedMovies.get(
+      `?api_key=${process.env.NEXT_PUBLIC_API_KEY}&query=${search}&page=1`,
+    )
     const responseMovies = await searchMovies.data.results
     setMoviesList(responseMovies)
   }
-  
-  async function getDefaultMovies(){
-    const responseMovies = await api.get(`popular?api_key=${process.env.NEXT_PUBLIC_API_KEY}`)
+
+  async function getDefaultMovies() {
+    const responseMovies = await api.get(
+      `popular?api_key=${process.env.NEXT_PUBLIC_API_KEY}`,
+    )
     const moviesData = await responseMovies.data.results
     setMoviesList(moviesData)
   }
 
   useEffect(() => {
-    if(!search){
+    if (!search) {
       getDefaultMovies()
-      return 
+      return
     } else {
       getSearchedMovies()
     }
-  },[search])
+  }, [search])
 
   return (
     <>
       <header className='bg-green-light-200 z-10 fixed w-full'>
         <div className='max-w-7xl w-full h-28 mx-auto px-4 flex flex-wrap sm:h-20 justify-between items-center'>
-          <div className=''>Movies</div>
+          <Link href='/'>
+            <a>Movies</a>
+          </Link>
           <div className='flex items-center w-96 px-2 bg-[#fff] rounded'>
             <input
               className='w-full h-10 px-2 border-none focus:outline-none'
